354f6babf8b1da4fff3d05c12dc8d279b08fe793
sunchang1996 <sunchang612@gmail.com>
2018-06-19 23:18:00 +0800

    feat: 作品空间页面

---
+++ src/components/Works.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="share-works-comments-wrap">
+    <router-link to="" target="_blank" class="works-item">
+      <div class="share-works-item" :style="{ height }">
+        <img src="https://output.xiguacity.cn/homework_screenshot/5ad82f2beb0a1971b138ec4d-c0a15cb991664f6794eea01c94c1d42d.jpg" width="120" height="90">
+        <p class="name" v-show="name">
+          {{name}}
+        </p>
+        <div class="share-works-status">
+          <div class="iconfont icon-like1">
+            <span>0</span>
+          </div>
+          <div class="iconfont eye icon-eye">
+            <span>0</span>
+          </div>
+        </div>
+        <div class="overlay-share-date">
+          <p class="info">06月19日</p>
+          <p class="info">十二课时分享</p>
+        </div>
+      </div>
+      <div class="comment-content">
+        <div class="content">
+          <p>好好学习，天天涨工资。好好学习，天天涨工资。好好学习，天天涨工资。好好学习，天天涨工资。好好学习，天天涨工资。好好学习，天天涨工资。</p>
+        </div>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'works',
+  props: {
+    name: String,
+  },
+  computed: {
+    height() {
+      return this.name ? '162px' : '132px'
+    }
+  },
+  mounted() {
+    console.log('-------',this.height)
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+.share-works-comments-wrap {
+  width: 100%;
+}
+.share-works-item {
+  position: relative;
+  display: inline-block;
+  width: 122px;
+  // height: 132px;
+  border: solid 1px #E2E9EB;
+  &:hover {
+    border: solid 1px #97AAAE;
+  }
+  > .name {
+    border-bottom: solid 1px #E2E9EB;
+    padding: 5px;
+    color: #607376;
+    text-align: center;
+  }
+  .share-works-status {
+    display: flex;
+    justify-content: space-between;
+    margin:10px;
+    > .eye {
+      color: #4DB141;
+    }
+  }
+  .overlay-share-date {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 120px;
+    height: 60px;
+    text-align: center;
+    background: rgba(240,240,240,0.75);
+    opacity: 0;
+    &:hover {
+      opacity: 1;
+    }
+    > .info {
+      padding: 2px;
+      margin: 0px;
+      font-size: 12px;
+      line-height: 15px;
+      color: #607376;
+    }
+  }
+}
+.comment-content {
+  padding: 20px;
+  width: 70%;
+  margin-top: 30px;
+  text-align: left;
+  display: inline-block;
+  vertical-align: top;
+  .content {
+    position: relative;
+    padding: 10px;
+    border-radius: 10px;
+    background-color: #A0E75A;
+    > p {
+      color: #607376;
+      font-size: 16px;
+    }
+    &::after {
+      position: absolute;
+      content: '';
+      top: 0;
+      left: -7px;
+      width: 7px;
+      height: 14px;
+      border-top-right-radius: 14px 7px;
+      border-right: 7px solid #A0E75A;
+    }
+  }
+}
+</style>
---
+++ src/views/Production.vue
@@ -0,0 +1,149 @@
+<template>
+<!-- 作品展示 -->
+  <div class="share-wrapper">
+    <div class="my-share-wrap">
+      <h1>我的作品分享</h1>
+      <div class="share-info">
+        <div class="statistic-container">
+          <h1>总计打开了</h1>
+          <div class="statistic-counting">
+            <span class="counting">18</span> 次
+          </div>
+          <div class="statistic-added">
+            <span>比昨天增加了</span> <span class="count">0</span> 次
+          </div>
+        </div>
+        <div class="statistic-container">
+          <h1>最近7天打开了</h1>
+          <div class="statistic-counting">
+            <span class="counting">9</span> 次
+          </div>
+          <div class="statistic-added">
+            <span>比昨天增加了</span> <span class="count">0</span> 次
+          </div>
+        </div>
+         <div class="statistic-container">
+          <h1>累积被点赞数</h1>
+          <div class="statistic-counting">
+            <span class="counting">0</span> 次
+          </div>
+          <div class="statistic-added">
+            <span>比昨天增加了</span> <span class="count">0</span> 次
+          </div>
+        </div>
+      </div>
+      <div class="my-share-wrap-works">
+        <p class="title"></p>
+        <div class="share-works">
+          <Works></Works>
+        </div>
+      </div>
+    </div>
+    <div class="other-share-wrap">
+      <h1>作品精选</h1>
+      <div class="switch-content">
+        <div class="switch" :class="{selected: selected}" @click="selected = !selected">本期</div>
+        <div class="switch" :class="{selected: !selected}" @click="selected = !selected">往期</div>
+      </div>
+      <div class="slected-homeword">
+        <Works name='快乐的小傻逼'></Works>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Works from '@/components/Works'
+
+export default {
+  name: 'production',
+  data() {
+    return {
+      selected: true
+    }
+  },
+
+  components: {
+    Works
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+.share-wrapper {
+  position: relative;
+  width: auto;
+  text-align: center;
+  min-width: 1055px;
+  display: flex;
+  justify-content: center;
+}
+.my-share-wrap, .other-share-wrap {
+  position: relative;
+  display: inline-block;
+  width: 500px;
+  height: 100%;
+  margin: 30px 10px 10px;
+  padding: 20px;
+  border: solid 1px #E2E9EB;
+  border-radius: 10px;
+  background: #f5f7f8;
+}
+.share-info {
+  display: flex;
+  padding-bottom: 30px;
+  .statistic-container {
+    flex: 1;
+    border-right: 1px solid #E2E9EB;
+  }
+  .statistic-container:last-child {
+    border: none;
+  }
+  h1 {
+    font-size: 16px;
+  }
+  .statistic-counting {
+    margin-top: 10px;
+    color: #4DB141;
+  }
+  .counting {
+    font-size: 36px;
+  }
+  .statistic-added {
+    margin-top: 10px;
+    font-size: 14px;
+    color: rgba(0,0,0,.65);
+    > .count {
+      color: #FF4000;
+    }
+  }
+}
+.my-share-wrap-works {
+  > .title {
+    margin-left: 100px;
+    margin-right: 100px;
+    padding-top: 10px;
+    border-top: solid 1px #E2E9EB;
+    text-align: center;
+    display: block;
+    position: relative;
+  }
+  > .share-works {
+    margin-top: 20px;
+  }
+}
+.switch-content {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+  .switch {
+    padding: 5px;
+    color: #607376;
+    cursor: pointer;
+  }
+  .selected {
+    color: #12B0C7;
+  }
+}
+</style>
